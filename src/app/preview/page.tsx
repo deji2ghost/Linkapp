ca7983eeccfd2fe785e230f8d662cfcfc3f4df49
deji <dejiwilliams9@gmail.com>
2025-02-10 23:18:33 +0100

    preview page.

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,9 +1,26 @@
+"use client"
+import { useFormContext } from '@/context/formContext'
+import Image from 'next/image'
 import React from 'react'
 
 const Preview = () => {
+    const { profileDetails, formLink } = useFormContext()
   return (
-    <div>
-      ball
+    <div className=''>
+        <div>
+            <Image alt='pic' src={profileDetails?.image} />
+            <h1>{profileDetails.firstName}</h1>
+            <p>{profileDetails.email}</p>
+        </div>
+        <div>
+            {
+                formLink.map((item, index) => (
+                    <div key={index}>
+                        {item.LinkName}
+                    </div>
+                ))
+            }
+        </div>
     </div>
   )
 }
